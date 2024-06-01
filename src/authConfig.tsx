eb7fb77e9cd44177f7975d7dfbbc990ae5823fd8
zekeriya kocairi <zkocairi@skillstown.com>
2024-06-01 13:05:51 +0200

    hide credentials

--- src/authConfig.tsx
+++ src/authConfig.tsx
@@ -4,8 +4,8 @@ import { Configuration } from '@azure/msal-browser';
 
 export const msalConfig: Configuration = {
     auth: {
-        clientId: '7f06f67b-f0a6-4361-9cf5-e3cc3796d7cb',//process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_ID as string,
-        authority: "https://login.microsoftonline.com/"+'95090b01-7b00-476f-8a70-c6820af4a2d1',//process.env.NEXT_PUBLIC_AZURE_AD_TENANT_ID,
+        clientId: '*****',//process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_ID as string,
+        authority: "https://login.microsoftonline.com/"+'*****',//process.env.NEXT_PUBLIC_AZURE_AD_TENANT_ID,
         redirectUri: 'http://localhost:3000'// process.env.NEXT_PUBLIC_AZURE_AD_REDIRECT_URI,
     },
     cache: {
@@ -45,5 +45,5 @@ export const loginRequest = {
 };
 
 export const tokenRequest = {
-    scopes: ["api://69ee91f6-1875-4423-a144-36c3e4b85a85/PrintMe.User"],
+    scopes: ["api://*****/PrintMe.User"],
 };
